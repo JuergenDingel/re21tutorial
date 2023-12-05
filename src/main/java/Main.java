9a156c2bf2d8f6d6a15fb3ab4827134eeac2b95f
JuergenDingel <38480087+JuergenDingel@users.noreply.github.com>
2023-12-05 14:30:00 -0500

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,7 @@
 public class Main {
 
 	public static void main(String[] args) {
-        System.out.println("Welcome to this tutorial!");
+        System.out.println("Welcome to THIS tutorial!");
 	}
 
 }
